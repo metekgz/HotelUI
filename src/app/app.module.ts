9c1aa030f2d7361f1ff0e5dd82df1333965eec2e
metekrgz <meteekrgz@gmail.com>
2023-06-12 14:12:25 +0300

    admin panel and sidebar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminModule } from './admin/admin.module';
 import { UserModule } from './user/user.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AdminModule, UserModule],
+  imports: [BrowserModule, AppRoutingModule, AdminModule, UserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
